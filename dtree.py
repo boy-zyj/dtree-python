ae7d9f143c977bcf937458d8bfd785bf09d60171
yaojunzou <yaodeshiluo@gmail.com>
2019-08-27 11:58:57 +0800

    add .then and .catch feature for Runner

--- dtree.py
+++ dtree.py
@@ -17,25 +17,25 @@ class NoMatchError(Error):
     pass
 
 
-def run_by_once_policy(self, data):
+def run_by_once_policy(self, obj):
     for cond, run in self._children.items():
-        if cond.validate(data):
-            return run.run(data)
+        if cond.validate(obj):
+            return run.run(obj)
     if self.else_:
-        return self.else_.run(data)
+        return self.else_.run(obj)
     else:
         raise NoMatchError
 
 
-def run_by_repeat_policy(self, data):
+def run_by_repeat_policy(self, obj):
     for cond, run in self._children.items():
         try:
-            if cond.validate(data):
-                return run.run(data)
+            if cond.validate(obj):
+                return run.run(obj)
         except NoMatchError:
             continue
     if self.else_:
-        return self.else_.run(data)
+        return self.else_.run(obj)
     else:
         raise NoMatchError
 
@@ -75,7 +75,7 @@ class Condition(_Base):
             return False
         return self._unique == other._unique
 
-    def validate(self, data):
+    def validate(self, obj):
         raise NotImplementedError
 
     def __or__(self, other):
@@ -94,8 +94,8 @@ class And(Condition):
     def __init__(self, *conds):
         self._conds = conds
 
-    def validate(self, data):
-        return all(cond.validate(data) for cond in self._conds)
+    def validate(self, obj):
+        return all(cond.validate(obj) for cond in self._conds)
 
     @property
     def _unique(self):
@@ -113,8 +113,8 @@ class Or(Condition):
     def __init__(self, *conds):
         self._conds = conds
 
-    def validate(self, data):
-        return any(cond.validate(data) for cond in self._conds)
+    def validate(self, obj):
+        return any(cond.validate(obj) for cond in self._conds)
 
     @property
     def _unique(self):
@@ -143,8 +143,8 @@ class Not(Condition):
             return (self.__class__, cond._unique)
         return cond._unique
 
-    def validate(self, data):
-        return not self._cond.validate(data)
+    def validate(self, obj):
+        return not self._cond.validate(obj)
 
     @property
     def description(self):
@@ -160,7 +160,7 @@ class Not(Condition):
 
 class Else(Condition):
 
-    def validate(self, data):
+    def validate(self, obj):
         return True
 
     def __eq__(self, other):
@@ -175,7 +175,7 @@ else_ = Else()
 
 class Runner(_Base):
 
-    def run(self, data):
+    def run(self, obj):
         raise NotImplementedError
 
     def __div__(self, runner):
@@ -185,6 +185,32 @@ class Runner(_Base):
         return Chain(*runners)
 
     __idiv__ = __div__
+
+    def then(self, next_runner):
+        return Chain(self, next_runner)
+
+    def catch(self, next_runner=None, error_handle=None):
+        return Capture(self, next_runner, error_handle)
+
+
+class Capture(Runner):
+
+    def __init__(self, pre_runner, next_runner=None, error_handle=None):
+        self.pre_runner = pre_runner
+        self.next_runner = next_runner
+        self.error_handle = error_handle
+
+    def run(self, obj):
+        try:
+            return self.pre_runner.run(obj)
+        except Exception as e:
+            if self.next_runner:
+                ret = self.next_runner.run(obj)
+            if self.error_handle:
+                self.error_handle(obj, e)
+                return ret
+            else:
+                raise e
 
 
 class Action(Runner):
@@ -198,10 +224,10 @@ class Chain(Action):
             assert isinstance(runner, Runner)
         self.runners = runners
 
-    def run(self, data):
+    def run(self, obj):
         ret = None
         for runner in self.runners:
-            ret = runner.run(data)
+            ret = runner.run(obj)
         return ret
 
     @property
@@ -297,11 +323,11 @@ class DTree(Runner):
     def else_(self):
         return self._else
 
-    def run(self, data):
+    def run(self, obj):
         run_method = POLICIES.get(self.policy)
         if run_method is None:
             raise UnknownPolicyError(self.policy)
-        return run_method(self, data)
+        return run_method(self, obj)
 
     def __str__(self):
         rv = ''
